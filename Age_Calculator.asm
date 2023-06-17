ccb2599147ca1d2d9ee7c8d0ab468812a9ca355f
parsa <parsagorgzan1225@gmail.com>
2023-06-17 22:52:06 +0330

    get day

--- Age_Calculator.asm
+++ Age_Calculator.asm
@@ -2,10 +2,18 @@
 
 .Data segment
 ; add your data here!
-WEL db  "Welcome$"
-YEAR db "Enter Your Birth Year (--xx):$"
-MON db  "Enter Your Birth Month (number):$"
-DAY db  "Enter Your Birth Day:$"
+M01 db  "Welcome$"
+M02 db "Enter Your Birth Year (--xx):$"
+M03 db  "Enter Your Birth Month (number):$"
+M04 db  "Enter Your Birth Day:$"
+
+day1    db ?
+day2    db ?
+month1  db ?
+month2  db ?
+year1   db ?
+year2   db ?
+
     
 ENDS
 
@@ -21,17 +29,83 @@ MOV DS, AX
 
 ; add your code here
 ; Day
-                
-LEA DX, WEL
-MOV AH, 9
-INT 21H        ; output string at ds:dx
-    
-; wait for any key....    
-MOV ah, 1
+MOV AH,2AH
 INT 21H
+MOV AL,DL
+AAM
+MOV BX,AX
+ 
+MOV AH,01H
+INT 21H
+SUB AL,48H
+MOV day2,AL
+
+INT 21H
+SUB AL,48H
+MOV day1,AL
+
+MOV AH,02H
+MOV DL,day2
+ADD DL,48H
+int 21h
+MOV DL,day1
+ADD DL,48H
+int 21h
+
+
+;CALL DISP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;Month Part
+;MOV AH,2AH    ; To get System Date
+;INT 21H
+;MOV AL,DH     ; Month is in DH
+;AAM
+;MOV BX,AX
+;CALL DISP
+
+;Year Part
+;MOV AH,2AH    ; To get System Date
+;INT 21H
+;ADD CX,0F830H ; To negate the effects of 16bit value,
+;MOV AX,CX     ; since AAM is applicable only for AL (YYYY -> YY)
+;AAM
+;MOV BX,AX
+;CALL DISP
+
+;MOV AH,4CH     ; To Terminate the Program
+;INT 21H
+                
+;LEA DX, WEL
+;MOV AH, 9
+;INT 21H        ; output string at ds:dx
     
-MOV ax, 4C00H ; exit to operating system.
-INT 21H    
-ENDS
+;Display Part
+;DISP PROC
+;MOV DL,BH      ; Since the values are in BX, BH Part
+;ADD DL,30H     ; ASCII Adjustment
+;MOV AH,02H     ; To Print in DOS
+;INT 21H
+;MOV DL,BL      ; BL Part 
+;ADD DL,30H     ; ASCII Adjustment
+;MOV AH,02H     ; To Print in DOS
+;INT 21H
+;RET
+;DISP ENDP      ; End Disp Procedure
 
 END START ; set entry point and stop the assembler.
