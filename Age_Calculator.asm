84740dc508c9a507edcae314c14261afa9327cda
parsa <parsagorgzan1225@gmail.com>
2023-06-17 23:27:16 +0330

    Get yy/mm/dd is Done

--- Age_Calculator.asm
+++ Age_Calculator.asm
@@ -3,16 +3,16 @@
 .Data segment
 ; add your data here!
 M01 db  "Welcome$"
-M02 db "Enter Your Birth Year (--xx):$"
-M03 db  "Enter Your Birth Month (number):$"
+M02 db  "Enter Your Birth Year:$"
+M03 db  "Enter Your Birth Month:$"
 M04 db  "Enter Your Birth Day:$"
 
-day1    db ?
-day2    db ?
-month1  db ?
-month2  db ?
-year1   db ?
-year2   db ?
+day1    db ?  ; (_X)
+day2    db ?  ; (X_)
+month1  db ?  ; (_X)
+month2  db ?  ; (X_)
+year1   db ?  ; (_X)
+year2   db ?  ; (X_)
 
     
 ENDS
@@ -28,22 +28,115 @@ MOV AX, @Data
 MOV DS, AX
 
 ; add your code here
-; Day
-MOV AH,2AH
+
+LEA DX, M01   ; Welcom Message
+MOV AH, 09H
 INT 21H
-MOV AL,DL
-AAM
-MOV BX,AX
- 
-MOV AH,01H
+
+MOV DL,0AH    ; New Line
+MOV AH,02H
 INT 21H
-SUB AL,48H
+
+MOV DL,0DH    ; Start New Line
+MOV AH,02H
+INT 21H
+
+;----------------------Year--------------------------
+
+LEA DX, M02   ; Enter Year Message
+MOV AH, 09H
+INT 21H
+
+
+; Get Year 
+MOV AH,01H    ; Get Year
+INT 21H
+SUB AL,48H    ;Convert Asci Value to Decimal
+MOV year2,AL
+
+INT 21H
+SUB AL,48H    ;Convert Asci Value to Decimal
+MOV year1,AL
+
+; Show Year
+MOV DL,0AH    ; New Line
+MOV AH,02H
+INT 21H
+
+MOV DL,0DH    ; Start New Line
+MOV AH,02H
+INT 21H
+
+MOV AH,02H
+MOV DL,year2
+ADD DL,48H
+int 21h
+
+MOV DL,year1
+ADD DL,48H
+int 21h
+
+;----------------------Month--------------------------
+
+MOV DL,0AH    ; New Line
+MOV AH,02H
+INT 21H
+
+MOV DL,0DH    ; Start New Line
+MOV AH,02H
+INT 21H
+
+LEA DX, M03   ; Get Month Message
+MOV AH, 09H
+INT 21H
+
+
+; Get Month
+MOV AH,01H    ; Get Month
+INT 21H
+SUB AL,48H    ;Convert Asci Value to Decimal
+MOV month2,AL
+
+INT 21H
+SUB AL,48H    ;Convert Asci Value to Decimal
+MOV month1,AL
+
+
+; Show Month
+MOV AH,02H
+MOV DL,month2
+ADD DL,48H
+int 21h
+MOV DL,month1
+ADD DL,48H
+int 21h 
+
+;----------------------Day-------------------------- 
+
+MOV DL,0AH    ; New Line
+MOV AH,02H
+INT 21H
+
+MOV DL,0DH    ; Start New Line
+MOV AH,02H
+INT 21H
+
+LEA DX, M04   ; Get Day Message
+MOV AH, 09H
+INT 21H
+
+; Get Day
+MOV AH,01H    ; Get Day
+INT 21H
+SUB AL,48H    ;Convert Asci Value to Decimal
 MOV day2,AL
 
 INT 21H
-SUB AL,48H
+SUB AL,48H    ;Convert Asci Value to Decimal
 MOV day1,AL
 
+
+; Show Day
 MOV AH,02H
 MOV DL,day2
 ADD DL,48H
@@ -53,6 +146,16 @@ ADD DL,48H
 int 21h
 
 
+MOV AH,4CH     ; To Terminate the Program
+INT 21H
+
+
+; Day
+MOV AH,2AH
+INT 21H
+MOV AL,DL
+AAM
+MOV BX,AX
 ;CALL DISP
 
 
