fa63435ec90d7f3876c2ae61a75992cfa31e1345
parsa <parsagorgzan1225@gmail.com>
2023-06-23 02:14:53 +0330

    change plan

--- Age_Calculator.asm
+++ Age_Calculator.asm
@@ -1,11 +1,7 @@
-; multi-segment executable file template.
-
-.Data segment
+.model small
+.Data
 ; add your data here!
 M01 db  "Welcome$"
-M02 db  "Enter Your Birth Year:$"
-M03 db  "Enter Your Birth Month:$"
-M04 db  "Enter Your Birth Day:$"
 M05 db  "Years:$"
 M06 db  "Months:$"
 M07 db  "Days:$"
@@ -23,6 +19,10 @@ monthS1  db ?
 monthS2  db ?
 yearS1   db ?
 yearS2   db ?
+
+year     db 02
+month    db 03
+day      db 27
 
 ageD2    db ?  ; Day
 ageD1    db ?  
@@ -46,104 +46,36 @@ MOV DS, AX
 
 ; add your code here
 
-LEA DX, M01   ; Welcom Message
-MOV AH, 09H
-INT 21H
+;LEA DX, M01   ; Welcom Message
+;MOV AH, 09H
+;INT 21H
 
-MOV DL,0AH    ; New Line
-MOV AH,02H
-INT 21H
+;MOV DL,0AH    ; New Line
+;MOV AH,02H
+;INT 21H
 
-MOV DL,0DH    ; Start New Line
-MOV AH,02H
-INT 21H
+;MOV DL,0DH    ; Start New Line
+;MOV AH,02H
+;INT 21H
 
 ;----------------------Year--------------------------
 
-LEA DX, M02   ; Enter Year Message
-MOV AH, 09H
-INT 21H
 
-
-; Get Year 
-MOV AH,01H    ; Get Year
-INT 21H
-SUB AL,30H    ;Convert Asci Value to Decimal 48
-MOV year2,AL
-
-INT 21H
-SUB AL,30H    ;Convert Asci Value to Decimal
-MOV year1,AL
+MOV DL, YEAR
 
 
 ;----------------------Month--------------------------
 
-MOV DL,0AH    ; New Line
-MOV AH,02H
-INT 21H
-
-MOV DL,0DH    ; Start New Line
-MOV AH,02H
-INT 21H
-
-LEA DX, M03   ; Get Month Message
-MOV AH, 09H
-INT 21H
 
 
-; Get Month
-MOV AH,01H    ; Get Month
-INT 21H
-SUB AL,48H    ;Convert Asci Value to Decimal
-MOV month2,AL
-
-INT 21H
-SUB AL,48H    ;Convert Asci Value to Decimal
-MOV month1,AL
-
+MOV DH, MONTH
 
 
 ;----------------------Day-------------------------- 
 
-MOV DL,0AH    ; New Line
-MOV AH,02H
-INT 21H
-
-MOV DL,0DH    ; Start New Line
-MOV AH,02H
-INT 21H
-
-LEA DX, M04   ; Get Day Message
-MOV AH, 09H
-INT 21H
-
-; Get Day
-MOV AH,01H    ; Get Day
-INT 21H
-SUB AL,48H    ;Convert Asci Value to Decimal
-MOV day2,AL
-
-INT 21H
-SUB AL,48H    ;Convert Asci Value to Decimal
-MOV day1,AL
 
 
-MOV DL,0AH    ; New Line
-MOV AH,02H
-INT 21H
-
-MOV DL,0DH    ; Start New Line
-MOV AH,02H
-INT 21H
-
-; Show Day
-;MOV AH,02H
-;MOV DL,day2
-;ADD DL,48H
-;int 21h
-;MOV DL,day1
-;ADD DL,48H
-;int 21h
+MOV BH, DAY
 
 
 
@@ -151,10 +83,15 @@ INT 21H
 
 MOV AH,2AH    ; To get System Date
 INT 21H
-MOV AL,DL     ; Day is in DL
-AAM
-MOV dayS2,AH
-MOV dayS1,AL
+
+; CX = Year
+; DH = Month
+; DL = Day
+
+
+LEA DX, M01   ; Welcom Message
+MOV AH, 09H
+INT 21H
 
 
 ; Show System Day
@@ -166,13 +103,13 @@ MOV DL,0DH    ; Start New Line
 MOV AH,02H
 INT 21H
 
-;MOV AH,02H
-;MOV DL,dayS2
-;ADD DL,30H
-;int 21h
-;MOV DL,dayS1
-;ADD DL,30H
-;int 21h
+MOV AH,02H
+MOV DL,dayS2
+ADD DL,30H
+int 21h
+MOV DL,dayS1
+ADD DL,30H
+int 21h
 
 MOV DL,'/'
 MOV AH,02H    ; To Print / in DOS
